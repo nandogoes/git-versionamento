c0b84baba342945f15c8838f5fcc02513ece5d49
Fernando Goes <nandogoes@gmail.com>
2020-03-22 19:38:21 -0300

    Corrigido arquivo index.

--- index.php
+++ index.php
@@ -8,14 +8,11 @@
         git-versionamento
         teste 2
         teste3
-<<<<<<< OURS
         teste 4
         test 5
         teste 6
         teste 7
         teste 8
-=======
->>>>>>> THEIRS
         <?php
         // put your code here
         ?>
