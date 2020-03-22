f7a4816d6440565bfb7556105f6c74b8fbd6198c
Fernando Goes <nandogoes@gmail.com>
2020-03-22 19:34:05 -0300

    Segundo Reset via Soft.

--- index.php
+++ index.php
@@ -9,6 +9,10 @@
         teste 2
         teste3
         teste 4
+        test 5
+        teste 6
+        teste 7
+        teste 8
         <?php
         // put your code here
         ?>
--- readme.md
+++ readme.md
@@ -1,3 +1,14 @@
 meu primeiro sistema no git.
 
 alterarção no readme, para teste do git.
+
+ola, tudo bem?
+
+
+
+
+
+
+
+
+steste
